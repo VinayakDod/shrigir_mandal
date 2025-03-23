e0ce45ae2fba6ee4bce238b45a0c99da2185a4e9
VinayakDod <vinayakdod@gmail.com>
2025-03-23 10:14:53 +0530

    Anil details added in donation

--- src/app/dashboard/donation/donation/donation.component.ts
+++ src/app/dashboard/donation/donation/donation.component.ts
@@ -8,6 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './donation.component.css'
 })
 export class DonationComponent {
-  names: string[] = ['Shivu Kambar', 'Vinayak B Dodakundi', 'Channabasappa Elagi', 'Guru Pujari ', 'Mallu Hiremath', 'Ganesh Hajeri', 'Naveen Jumnal', 'Arun Gadagi', 'Appu Jumnal', 'Modi Guru', 'Prashant Gadagi', 'Pundu (Ganvdi)', 'Puttu T', 'Sachin Gadagi', 'Prakash Dodimanni', 'Shivu ', 'Basavaraj Jumanal', 'Swami Chikkamt', 'Guru Bijjaragi', 'Ishwar Kabade', 'Jagdish', 'Mallikarjun Manashetti', 'Malingaray Toravi',];
+  names: string[] = ['Shivu Kambar', 
+    'Vinayak B Dodakundi', 
+    'Channabasappa Elagi', 
+    'Guru Pujari ', 
+    'Mallu Hiremath', 
+    'Ganesh Hajeri', 
+    'Naveen Jumnal', 
+    'Arun Gadagi', 
+    'Appu Jumnal', 
+    'Modi Guru', 
+    'Prashant Gadagi', 
+    'Pundu (Ganvdi)', 
+    'Puttu T', 
+    'Sachin Gadagi', 
+    'Prakash Dodimanni', 
+    'Shivu ', 
+    'Basavaraj Jumanal', 
+    'Swami Chikkamt', 
+    'Guru Bijjaragi', 
+    'Ishwar Kabade', 
+    'Jagdish', 
+    'Mallikarjun Manashetti', 
+    'Malingaray Toravi',];
 
 }
